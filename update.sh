dca646f8cfbb9b3989f927d09573f8abde620ed9
grapscript <opensource578@gmail.com>
2024-07-25 01:03:44 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -36,7 +36,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/QuinStore/scpremium/main/Cdy/menu.zip
+    wget https://raw.githubusercontent.com/QuinStore/pro/main/Cdy/menu.zip
     chmod +x menu/*
     enc menu/*
     mv menu/* /usr/local/sbin
@@ -55,7 +55,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPTi        \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPTk        \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
