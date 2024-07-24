f8355ea4dcde599f4ca96fe699196b4f6b72ba0d
grapscript <opensource578@gmail.com>
2024-07-25 01:08:45 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-clear
-cd /usr/local/
-rm -rf sbin
-rm -rf /usr/bin/enc
-cd
-mkdir /usr/local/sbin
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+
 clear
 fun_bar() {
     CMD[0]="$1"
@@ -19,10 +10,10 @@ fun_bar() {
         touch $HOME/fim
     ) >/dev/null 2>&1 &
     tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     while true; do
-        for ((i = 0; i < 10; i++)); do
-            echo -ne "\033[0;32m# "
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
             sleep 0.1s
         done
         [[ -e $HOME/fim ]] && rm $HOME/fim && break
@@ -30,28 +21,31 @@ fun_bar() {
         sleep 1s
         tput cuu1
         tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+        echo -ne "\033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     done
     echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/QuinStore/pro/main/Cdy/menu.zip
+    wget https://raw.githubusercontent.com/grapscript/pro/main/Cdy/menu.zip
+    unzip menu.zip
     chmod +x menu/*
-    enc menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu
     rm -rf menu.zip
+    rm -rf update.sh
 }
 netfilter-persistent
 clear
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPTk        \e[0m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 echo -e ""
-echo -e "  \033[1;91m update script service\033[1;37m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e "\e[1;97;101m            » UPDATE SCRIPT «             \033[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+echo -e "\033[1;91mUpdate Script Service\033[1;37m"
 fun_bar 'res1'
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e ""
-read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+read -n 1 -s -r -p "Press [ Enter ] To Back On Menu"
 menu
